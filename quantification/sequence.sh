c078d12f67c04da0a85a8a493a6b6f51fa358847
Alexander de Wolff <alex.jn.wilcox@gmail.com>
2018-04-10 21:15:24 -0700

    fixed sequence.sh
    fixed filename errors

--- quantification/sequence.sh
+++ quantification/sequence.sh
@@ -4,7 +4,7 @@ echo "Processing sample ${i}"
 # Assuming salmon is one directory above the reads directory, ie
 # in the directory where salmon resides there is a directory reads.
 salmon quant -i athal_index -l A \
-         -r ./reads/${i}_1.fastq.gz \
-            ./reads/${i}_2.fastq.gz \
+         -r reads/DRR0161${i}_1.fastq.gz \
+            reads/DRR0161${i}_2.fastq.gz \
          -p 8 --gcBias -o quants/${i}_quant
 done 
